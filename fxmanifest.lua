fe7e024c278d1da260bb8ccc85cfa9186f291a1f
CrypticTM99 <67986767+CrypticTM99@users.noreply.github.com>
2022-09-16 19:20:40 -0600

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+author 'case#2506'
+
+description 'case-whitewidowjob'
+
+version '0.1'
+
+shared_scripts {
+    'config.lua',
+}
+
+client_scripts{
+    'client/*.lua',
+}
+
+server_scripts{
+    'server/*.lua',
+}
